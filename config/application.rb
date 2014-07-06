3d277fc004240e76c5df778ef3456c8d5bc54fc4
-l <latem999@o2.pl>
2014-07-06 23:42:48 +0200

    Enforce available locales

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,10 @@ module Cmentarz
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :pl
+    
     config.sass.preferred_syntax = :sass
   end
 end
