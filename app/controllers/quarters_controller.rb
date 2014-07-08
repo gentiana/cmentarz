1b0c500ecf8e11751ee6d1a34064bca78b09d48e
-l <latem999@o2.pl>
2014-07-08 11:08:14 +0200

    Add preliminary quarters controller

--- app/controllers/quarters_controller.rb
+++ app/controllers/quarters_controller.rb
@@ -1,13 +1,36 @@
 class QuartersController < ApplicationController
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
   def index
+    @quarters = Quarter.all
+  end
+  
+  def show
+    @quarter = Quarter.find(params[:id])
+  end
+  
+  def new
+    @quarter = Quarter.new
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    @quarter = Quarter.find(params[:id])
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    Quarter.find(params[:id]).destroy
+  end
+  
+  
+  private
+  
+  def quarter_params
+    params.require(:quarter).permit(:name, :short_name)
   end
 end
